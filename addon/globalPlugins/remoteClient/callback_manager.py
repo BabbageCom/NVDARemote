1954306f2c2324479209f0e4afa8760b389e8fcf
Leonard de Ruijter <leonard@babbage.com>
2018-05-09 07:30:00 +0200

    use extensionPoints for callback handler

--- addon/globalPlugins/remoteClient/callback_manager.py
+++ addon/globalPlugins/remoteClient/callback_manager.py
@@ -2,30 +2,23 @@ from logging import getLogger
 logger = getLogger('callback_manager')
 import wx
 from collections import defaultdict
+from extensionPoints import Action, callWithSupportedKwargs
 
 class CallbackManager(object):
 	"""A simple way of associating multiple callbacks to events and calling them all when that event happens"""
 
 	def __init__(self):
-		self.callbacks = defaultdict(list)
+		self.callbacks = defaultdict(Action)
 
 	def register_callback(self, event_type, callback):
 		"""Registers a callback as a callable to an event type, which can be anything hashable"""
-		self.callbacks[event_type].append(callback)
+		self.callbacks[event_type].register(callback)
 
 	def unregister_callback(self, event_type, callback):
 		"""Unregisters a callback from an event type"""
-		self.callbacks[event_type].remove(callback)
+		self.callbacks[event_type].unregister(callback)
 
 	def call_callbacks(self, type, *args, **kwargs):
 		"""Calls all callbacks for a given event type with the provided args and kwargs"""
-		for callback in self.callbacks[type]:
-			try:
-				wx.CallAfter(callback, *args, **kwargs)
-			except Exception as e:
-				logger.exception("Error calling callback %r" % callback)
-		for callback in self.callbacks['*']:
-			try:
-				wx.CallAfter(callback, type, *args, **kwargs)
-			except Exception as e:
-				logger.exception("Error calling callback %r" % callback)
+		self.callbacks[type].notify(*args, **kwargs)
+		self.callbacks["*"].notify(*args, **kwargs)
